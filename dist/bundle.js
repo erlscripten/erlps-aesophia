ed27222ee0a5a131ab25d8b058ee8d06bf4adce4
Grzegorz Uriasz <gorbak25@gmail.com>
2021-03-01 21:29:28 +0100

    bundling

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['erlps-aesophia'] = factory());
+}(this, (function () { 'use strict';
+
+	var dist = PS;
+
+	return dist;
+
+})));
